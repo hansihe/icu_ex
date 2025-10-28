ea9eb3ae483a420164781e815966d45d9c09e9ff
Hans Josephsen <hans@bukholm.com>
2025-10-28 13:37:08 +0100

    add NIF prebuild

--- lib/icu/nif.ex
+++ lib/icu/nif.ex
@@ -1,7 +1,38 @@
 defmodule Icu.Nif do
-  use Rustler,
+  @doc false
+
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+  github_url = mix_config[:package][:links]["GitHub"]
+
+  # Since Rustler 0.27.0, we need to change manually the mode for each env.
+  # We want "debug" in dev and test because it's faster to compile.
+  mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
+
+  use RustlerPrecompiled,
     otp_app: :icu,
-    crate: :icu_nif
+    crate: :icu_nif,
+    version: version,
+    base_url: "#{github_url}/releases/download/v#{version}",
+    targets: ~w(
+      aarch64-apple-darwin
+      aarch64-unknown-linux-gnu
+      aarch64-unknown-linux-musl
+      x86_64-apple-darwin
+      x86_64-pc-windows-msvc
+      x86_64-pc-windows-gnu
+      x86_64-unknown-linux-gnu
+      x86_64-unknown-linux-musl
+      x86_64-unknown-freebsd
+    ),
+    # We don't use any features of newer NIF versions, so 2.15 is enough.
+    nif_versions: ["2.15"],
+    mode: mode,
+    force_build: System.get_env("ICU_BUILD") in ["1", "true"]
+
+  # use Rustler,
+  #   otp_app: :icu,
+  #   crate: :icu_nif
 
   def locale_from_string(_locale_string), do: :erlang.nif_error(:nif_not_loaded)
   def locale_to_string(_resource), do: :erlang.nif_error(:nif_not_loaded)
